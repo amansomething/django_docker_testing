818c36ca69033ce172fd4edffdff30c4a90a17f3
aman <amansomething@gmail.com>
2024-05-23 19:53:34 -0400

    updated dummy model

--- dummy/models.py
+++ dummy/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class DummyModel(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    active = models.BooleanField(default=True)
