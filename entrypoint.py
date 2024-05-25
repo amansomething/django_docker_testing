bc7d3a3a8df9f927230c272eda9e5c1ae511cf95
aman <amansomething@gmail.com>
2024-05-25 10:40:15 -0400

    refactor: entrypoint using pydantic. black formatting.

--- entrypoint.py
+++ entrypoint.py
@@ -1,36 +1,55 @@
 import os
 import secrets
 import string
-import sys
 import subprocess
-from typing import NamedTuple
-from collections import namedtuple
+import sys
+from typing import Union
 
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 load_dotenv()
 
 
-RequiredPW = namedtuple("RequiredPW", ["var", "length", "special_chars"])
+class RequiredPW(BaseModel):
+    """
+    Pydantic model for required password variables.
 
+    :param name: Name of the environment variable. All caps, snake_case recommended.
+    :param length: Desired length of the password. Default = 64
+    :param special_chars: Special characters to allow in the password. Default = "!@#^*()"
+
+    Example Definitions:
+        RequiredPW(name="SECRET_KEY")
+        RequiredPW(name="PASSWORD", length=16)
+        RequiredPW(name="YET_ANOTHER_SECRET_KEY", special_chars="!@#^*()")
+        RequiredPW(name="SuperCustom", length=128, special_chars="!")
+    """
+    name: str
+    length: int = 64
+    special_chars: str = "!@#^*()"
+
+
+# See the RequiredPW class for details.
 REQUIRED_PW_VARS = (
-    RequiredPWVar("DJANGO_SECRET_KEY", 0),
-    RequiredPWVar("DJANGO_SUPERUSER_PASSWORD", 16),
-    RequiredPWVar("YET_ANOTHER_SECRET_KEY", 0),
+    RequiredPW(name="DJANGO_SECRET_KEY"),
+    RequiredPW(name="DJANGO_SUPERUSER_PASSWORD", length=16),
+    RequiredPW(name="YET_ANOTHER_SECRET_KEY"),
 )
 OTHER_REQUIRED_VARS = (
-    "DATABASE_URL",
-    "REDIS_URL",
+    "VAR_1",
+    "VAR_2",
     "SOME_OTHER_VAR",
 )
+ENV_FILE = ".env"
 
 
-def generate_password(length: int = 64, special_chars: str = "!@#^*()") -> str:
+def generate_password(length: int = 64, special_chars: str = "") -> str:
     """
     Generate a random password with letters, digits, and special characters.
 
     :param length: Desired length of the password. Default = 64
-    :param special_chars: Special characters to include in the password. Default = "!@#^*()"
+    :param special_chars: Special characters to include in the password. Default = ""
     :return: A random password of the specified length.
     """
     alphabet = string.ascii_letters + string.digits + special_chars
@@ -39,69 +58,81 @@ def generate_password(length: int = 64, special_chars: str = "!@#^*()") -> str:
     return password
 
 
-def check_missing_vars(vars_: tuple[str, ...]) -> list[str]:
+def check_pw_vars(
+        required_pw_vars: tuple[RequiredPW] = REQUIRED_PW_VARS, env_file: str = ENV_FILE
+) -> None:
     """
-    Checks if given variables are available.
-    If not, print a list of missing variables.
-
-    :param vars_: Tuple of required variables.
-    :return: List of missing variables.
-    """
-    env_vars = os.environ.keys()
-    missing_vars = [var for var in vars_ if var not in env_vars]
-
-    if missing_vars:
-        print("The following required variables are missing:")
-        for var in missing_vars:
-            print(f"- {var}")
-        print()
-
-    return missing_vars
-
-
-def check_pw_vars(required_pw_vars: tuple[str, ...] = REQUIRED_PW_VARS) -> None:
-    """
-    Checks if required password variables are available.
+    Checks if required password variables are loaded.
     If not, generates them and adds them to the .env file.
+    Prints out status messages as it goes.
 
-    :param required_pw_vars: Tuple of required password variables.
+    :param required_pw_vars: Tuple of RequiredPW variables to check.
+    :param env_file: Env file name to write passwords to.
     :return: None
     """
-    missing_vars = check_missing_vars(required_pw_vars)
+    missing_vars = []
+    for var in required_pw_vars:
+        if not os.getenv(var.name):
+            print(f"Missing required password variable: {var.name}")
+            missing_vars.append(var)
 
     if not missing_vars:
         return
 
-    print("Generating missing password variables...")
-    pw_vars = []
+    print("\nGenerating missing password variables...")
+    pw_vars = ["# Missing Required Passwords\n"]
     for var in missing_vars:
-        print(f"Generating password for {var}...")
-        password = generate_password()
-        pw_vars.append(f'{var}="{password}"\n')
+        print(f"Generating password for {var.name}...")
+        password = generate_password(var.length, var.special_chars)
+        pw_vars.append(f'{var.name}="{password}"\n')
+    pw_vars.append("\n")
 
-    print("Writing password variables to .env file...")
-    with open(".env", "a") as f:
+    print("\nAppending password variables to .env file...")
+    with open(env_file, "a") as f:
         f.writelines(pw_vars)
-    print()
+    print("Done!\n")
 
 
-def check_required_vars(required_vars: tuple[tuple[str, int], ...] = OTHER_REQUIRED_VARS) -> None:
+def check_required_vars(
+        required_vars: Union[list[str], tuple[str, ...]] = OTHER_REQUIRED_VARS,
+        env_file: str = ENV_FILE,
+) -> None:
     """
     Checks if required variables are available and not empty.
-    If any variables are missing or empty print out the errors and exit the program.
+    If variable are missing:
+        - Adds them to the .env file as empty strings.
+        - Prints out a message to fill them out.
+        - Exits the program.
+    If variables are empty strings, prints out the errors and exits the program.
+    Prints out status messages as it goes.
 
-    :param required_vars: Tuple of required variables.
+    :param required_vars: List or tuple of required variables to check.
+    :param env_file: Env file name to write variables to.
     :return: None
     """
-    missing_vars = check_missing_vars(required_vars)
+    missing_vars = []
+    for var in required_vars:
+        if not os.getenv(var):
+            print(f"Missing required variable: {var}")
+            missing_vars.append(var)
+
     if missing_vars:
+        print("\nAppending missing variables to .env file...")
+        required_vars = ["\n# Required Variables\n"]
+        for var in missing_vars:
+            required_vars.append(f'{var}=""\n')
+
+        with open(env_file, "a") as f:
+            f.writelines(required_vars)
+
+        print("Done! Fill those out and re-run the script.")
         sys.exit(1)
 
     empty_vars = [var for var in required_vars if not os.environ.get(var)]
     if empty_vars:
         print("The following required variables are empty:")
         for var in empty_vars:
-            print(f"\t- {var}")
+            print(f"- {var}")
         sys.exit(1)
 
     print("All required variables are set and not empty.")
--- start_app.sh
+++ start_app.sh
@@ -3,11 +3,13 @@
 echo "Prepping Django App..."
 python manage.py makemigrations dummy  # Create migrations for the dummy app
 python manage.py migrate  # Apply migrations to the database
+echo "Done!"
 echo
 
 echo "Creating Superuser..."
 DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" \
 python manage.py createsuperuser --noinput --username=admin --email=test@test.com
+echo "Done!"
 echo
 
 echo "Starting Django App..."
